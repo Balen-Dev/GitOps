bf0114eab72e3881d481d03cedd55dd631e4bfe0
Balen <contact@balen.dev>
2025-07-09 15:41:20 +0300

    Changed root route message

--- services/go/api.go
+++ services/go/api.go
@@ -31,7 +31,7 @@ func generalGreetingHandler(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    fmt.Fprintln(w, "Hello! Welcome to the GO API (Updated by FluxCD).")
+    fmt.Fprintln(w, "Hello! Welcome to the GO API (Deployed by FluxCD).")
 }
 
 func customGreetingHandler(w http.ResponseWriter, r *http.Request) {
